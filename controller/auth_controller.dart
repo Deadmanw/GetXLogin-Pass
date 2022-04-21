2c66d1619499203f4918e750f1a29c99bb6cb213
Deadmanw <84095536+Deadmanw@users.noreply.github.com>
2022-04-20 22:02:09 -0500

    Add files via upload

---
+++ controller/auth_controller.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:get/get.dart';
+import 'package:pass_/repository/auth_repository.dart';
+import 'package:pass_/repository/routes/routes.dart';
+
+enum AuthState {
+  signedOut,
+  signedIn,
+}
+
+class AuthController extends GetxController {
+  final _authRepository = Get.find<AuthRepository>();
+  late StreamSubscription _authSubscription;
+
+  final Rx<AuthState> authState = Rx(AuthState.signedOut);
+  final Rx<AuthUser?> authUser = Rx(null);
+
+  @override
+  void onInit() async {
+    // Just for testing. Allows the splash screen to be shown a few seconds
+    await Future.delayed(const Duration(seconds: 3));
+    _authSubscription =
+        _authRepository.onAuthStateChanged.listen(_authStateChanged);
+    super.onInit();
+  }
+
+  void _authStateChanged(AuthUser? user) {
+    if (user == null) {
+      authState.value = AuthState.signedOut;
+      Get.offAllNamed(Routes.intro);
+    } else {
+      authState.value = AuthState.signedIn;
+      Get.offAllNamed(Routes.home);
+    }
+    authUser.value = user;
+  }
+
+  Future<void> signOut() async {
+    await _authRepository.signOut();
+  }
+
+  @override
+  void onClose() {
+    _authSubscription.cancel();
+    super.onClose();
+  }
+}
